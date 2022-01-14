61056ce893ade03a9dab6e54564b05b49dacc04e
kedmonmaquiz <jkedmon95@gmail.com>
2022-01-14 21:12:22 +0300

    Built a method to calculate atm distance from current location

--- ATMLocator.java
+++ ATMLocator.java
@@ -1,6 +1,10 @@
 
 public class ATMLocator {
     
+    private double getLocation(double c,double a){	
+		return a - c;
+	}
+    
     //Main function begin
 	public static void main(String[] args){
 		//
