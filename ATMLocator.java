53951485147fcb851c2a6215d803ea1c118430b5
kedmonmaquiz <jkedmon95@gmail.com>
2022-01-14 21:16:49 +0300

    performing logics to compute your current location and ATM near you

--- ATMLocator.java
+++ ATMLocator.java
@@ -23,7 +23,30 @@ public class ATMLocator {
 		double current_location = 0.00;
 
 		Map<String,Double> allATMLocations = new HashMap<String,Double>();
-        
+
+        //Map of ATM names and their distance coordinates
+		allATMLocations.put("atm1",56.8);
+		allATMLocations.put("atm2",42.4);
+		allATMLocations.put("atm3",35.7);
+		allATMLocations.put("atm4",61.5);
+		allATMLocations.put("atm5",57.0);
+		allATMLocations.put("atm6",32.2);
+		allATMLocations.put("atm7",35.1);
+		allATMLocations.put("atm8",5.3);
+		
+		allATMLocations.forEach((atm,distance) ->{
+		    if(pq.size() < number_of_ATMs){
+			pq.add(aL.getLocation(current_location,distance));}
+		else{
+			if(	pq.peek() > aL.getLocation(current_location,distance)){
+				pq.poll();
+				pq.add(aL.getLocation(current_location,distance));
+			   }
+	      	}
+		});
+
+		pq.forEach(atmLocation -> System.out.println(atmLocation));	
+
 	}
 	
 }
